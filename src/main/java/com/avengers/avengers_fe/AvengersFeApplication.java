8dd0bf57c5fb8721b76f4c50bbccbeeccc0b7c68
Md Fahad Hasan <mdashfahad@gmail.com>
2024-08-01 21:17:09 +0200

    feat: Add file src/main/java/com/avengers/avengers_fe/AvengersFeApplication.java

---
+++ src/main/java/com/avengers/avengers_fe/AvengersFeApplication.java
@@ -0,0 +1,13 @@
+package com.avengers.avengers_fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengersFeApplication.class, args);
+	}
+
+}
